ded1a96bf31cfaddffc451c00e46f02cddd13dbd
deepnetni <deepnetni@163.com>
2023-05-21 11:17:40 +0800

    use relative line number

--- config.el
+++ config.el
@@ -54,6 +54,7 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+(menu-bar--display-line-numbers-mode-relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
