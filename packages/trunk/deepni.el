97caac79fa4a6522ce111b3127f291d9fdc33d87
niye <deepnetni@163.com>
2025-09-04 00:18:27 +0800

    init

---
+++ packages/trunk/deepni.el
@@ -0,0 +1,91 @@
+;;; trunk/deepni.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Bind all the settings to this minor mode for easy management
+
+(require 'persp-mode)
+(require 'hideif)
+(require 'format-all)
+(require 'evil)
+;;(require 'ivy)
+
+;;; Code:
+
+;;;###autoload
+(defmacro deepni/goto-center (args)
+  "Move the line outputed by ARGS to center."
+  `(dolist (tgt ',args)
+     (advice-add tgt :after (lambda (&rest _)
+                              (evil-scroll-line-to-center (line-number-at-pos))))))
+
+;;;###autoload
+(defun deepni/format-code ()
+  "Process macros first in c/cpp mode before format."
+  (interactive)
+  (pcase major-mode
+    ('c-mode (hide-ifdefs)
+             (+format/buffer))
+    ('c++-mode (hide-ifdefs)
+               (+format/buffer))
+    ('python-mode (+format/buffer))
+    (_ (+format/buffer))))
+
+;;;###autoload
+(defun deepni/add-remove-backends (opt backend)
+  (when (boundp 'company-backends)
+                                        ;(make-local-variable 'company-backends)
+    (message "### in %s" company-backends)
+    (pcase opt
+      ;; add
+      ('add (add-to-list 'company-backends backend))
+      ;; remove
+      ('remove
+       (setq company-backends (delete backend company-backends))))
+    (message "### out %s" company-backends)
+    ))
+
+(defun deepni/save-layout ()
+  "Save current layout to file."
+  (interactive)
+  (persp-save-state-to-file (concat persp-save-dir "deep-layout")))
+
+
+(defun deepni/dired-do-rename (arg)
+  "rename file without company."
+  (interactive "P")
+  (let ((old-name (dired-get-filename)))
+    (setq new-name (read-string "Rename to: " old-name))
+    (dired-rename-file old-name new-name)))
+
+(defun deepni/load-layout ()
+  "Load last time layout from file."
+  (interactive)
+  (let ((fname (concat persp-save-dir "deep-layout")))
+    (message "### %s %s" fname (file-exists-p fname))
+    (when (file-exists-p fname)
+      (persp-load-state-from-file fname))))
+
+(defun deepni/python-current-file ()
+  "insert current filename in compile buffer."
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if file-name
+        (compile (format "python %s" file-name))
+      (message "No file associated with this buffer."))))
+
+
+;; NOTE define minor mode
+
+(defvar deepni-mode-map (make-sparse-keymap)
+  "Keymap used in `deepni-mode' buffers.")
+
+;;;###autoload
+(define-minor-mode deepni-mode
+  "A minor mode for deepnetni to override conflict settings."
+  :init-value t
+  :lighter ""
+  :keymap deepni-mode-map)
+
+(provide 'deepni)
+;;; deepni.el ends here
---
+++ packages/trunk/test.el
@@ -0,0 +1,148 @@
+;;; trunk/test.el -*- lexical-binding: t; -*-
+
+;;; Code:
+
+
+(require 'deepni)
+
+(message "test deepni package")
+
+(defun print-name (&rest args)
+  (let ((first (plist-get args :first))
+        (key (or (plist-get args :key) "?")))
+    (message key)
+    (when first
+      (message first))))
+
+(defun print-name_1 (&rest args)
+  (let ((first (plist-get args :first))
+        (key (or (plist-get args :key) "?")))
+    (message key)
+    (when first
+      (message first))))
+
+(setq funcs #'(print-name print-name_1))
+
+(defun fadd (f-alist)
+  (dolist (f f-alist)
+    (message "%s" (symbolp `#',f))
+    (message "%s start" `#',f)
+    (advice-add `#',f :after
+                (lambda (&rest _)
+                  (message "## after done")))))
+
+                                        ;(fadd #'(print-name print-name_1))
+(advice-add 'print-name :after
+            (lambda (&rest _)
+              (message "## after done")))
+
+(defmacro tm (args)
+  `(dolist (tgt ',args)
+     (message "%d" tgt)))
+
+(tm (1 2 3))
+
+                                        ;(print-name :first "first arg" :key "this is key")
+(window-system)
+
+(system-name)
+
+(display-graphic-p)
+
+(message "%s" IS-MAC)
+
+(message "%s" IS-WINDOWS)
+
+(message "%s" IS-BSD)
+
+(message "%s" IS-LINUX)
+
+
+(defun testpcase (arg)
+  (pcase arg
+    (1 (message "1"))
+    (2 (message "2"))
+    (_ (message "others"))))
+
+(testpcase 4)
+
+(unless nil
+  (message "not nil"))
+
+
+(when nil
+  (message "t"))
+
+
+(cond
+ (IS-MAC (message "mac"))
+ (IS-LINUX (message "linux"))
+ (IS-WINDOWS (message "windows"))
+ )
+
+(setq a '(1 2 3))
+
+(push '(4 3) a)
+
+
+(delete 'company-capf company-backends)
+
+(setq company-backends (delete 'company-capf company-backends))
+
+(message "%s" company-backends)
+
+(defun deepni/add-remove-backends (opt backend)
+  (when (boundp 'company-backends)
+    (message "in %s" company-backends)
+                                        ;(make-local-variable 'company-backends)
+    (pcase opt
+      ;; add
+      ('add (add-to-list 'company-backends backend)
+            (message "out %s" company-backends)
+            )
+      ;; remove
+      ('remove
+       (setq company-backends (delete backend company-backends))))
+    ))
+
+(deepni/add-remove-backends 'add 'company-capf)
+
+(deepni/add-remove-backends 'remove 'company-capf)
+
+
+(message "%s" company-backends)
+
+
+(file-exists-p "~/anaconda3/envs/work3")
+
+
+(defun test-interactive (arg)
+  (interactive "bbuffer-name:")
+  (message arg))
+
+(expand-file-name (buffer-name))
+
+(defun deepni/run-current-python-file (fname)
+  "Run current python file pointed by `FNAME'."
+  (interactive "bRun File: ")
+  (message fname)
+  (message (expand-file-name (buffer-name)))
+  (+ivy/compile)
+  (buffer-name)
+  )
+
+
+;(call-interactively #'+lookup/online)
+
+(file-name-extension (buffer-name) t)   ; .el
+(file-name-extension (buffer-name))     ; el
+
+
+(string-suffix-p "pre" "hello.prex")    ; nil
+(string-suffix-p "pre" "hello.pre")     ; t
+
+(print noninteractive)
+
+
+
+(list '(a) '(1 2) '(c))
